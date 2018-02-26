339f2348581a6fd4dfe5c1caf5187405f0ab52d6
stpangia <sean@redcent.net>
2018-02-26 17:07:08 -0500

    Saving mobile number

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,7 @@ exports.oauth_handshake = function(req, res) {
 
 exports.user = function(req, res) {
 	// Initialize user's device via adapter
-  var device = deviceAdapter.getDevice(req.params.device);
+  var device = deviceAdapter.getDevice(req.session.device.type);
 
   // Get user
 	device.getUser(req.session.device.user_token)
@@ -57,9 +57,22 @@ exports.user = function(req, res) {
     });
 };
 
-
 exports.update = function(req, res) {
-
+	// sanitize input
+	var mobile = req.body.mobile.replace(/[^0-9]/g,'');
+	// validate input 
+	if (mobile.length != 10) {
+		res.render('user', { flash: {status:'warn', message:'Invalid cell number. Enter a 10-digit US phone number.'} }); 
+	}
+	// Save user
+	var dbPromise = Promise.resolve()
+		.then(() => sqlite.open('./database.sqlite', { Promise }))
+		.then(function(db){
+			db.run('UPDATE users SET mobile = ? WHERE device_user_id = ?', [mobile, req.session.device.user_token])
+			.then(function (result) {
+				res.render('user', { flash: {status:'success', message:'User profile updated'} }); 
+			})
+		});			
 };	
 
 exports.logout = function(req, res) {
--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.get('/oauth-redirect/:device', user_controller.oauth_handshake);
 /* GET User. */
 router.get('/user', user_controller.user);
 
+/* POST User save/update. */
+router.post('/user', user_controller.update);
 
 /* GET Logout and forget token (Should be POST but this is just a demo project). */
 router.get('/logout', user_controller.logout);
