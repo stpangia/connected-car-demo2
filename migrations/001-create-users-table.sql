9dc2827004efc242d04ef71d48ee5f331c89d174
stpangia <sean@redcent.net>
2018-03-12 12:22:15 -0400

    added timestamp for monthly alert limit

--- migrations/001-create-users-table.sql
+++ migrations/001-create-users-table.sql
@@ -3,7 +3,8 @@ CREATE TABLE users (
 	id	INTEGER PRIMARY KEY,
   device_user_id	TEXT,
   mobile	TEXT,
-  opt_in	NUMERIC DEFAULT 1
+  opt_in	NUMERIC DEFAULT 1,
+  alerted_at	TIMESTAMP
 );
 -- Down
 DROP TABLE users;
